4a3f00a8c5de0d4e034e69818483adf2acaab90d
Doug Harple <dharple@gmail.com>
2021-02-01 01:22:37 -0500

    Add test for bug with empty default eating up valid characters (resolves #19)

---
+++ tests/tests/github-issue-0019/test.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -e
+#
+# Tests to confirm GitHub issue #19 is resolved
+#
+# https://github.com/dharple/detox/issues/19
+#
+
+if [ -z "$TESTBASE" ] ; then
+	echo TESTBASE needs to be passed via the env
+	exit 1
+fi
+
+. $TESTBASE/test-functions
+
+# Run the test based on data from the issue
+
+DETOX=$1
+INPUT="01 5G Core Networks.pdf"
+OUTPUT="01 5G Core Networks.pdf"
+METHOD="safe"
+TABLE="test.tbl"
+
+test_single_table "$DETOX" "$INPUT" "$OUTPUT" "$METHOD" "$TABLE"
