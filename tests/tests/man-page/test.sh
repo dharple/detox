629c1d6f49bafab32cd77046378590b4bedd1374
Doug Harple <dharple@gmail.com>
2021-02-15 03:16:35 -0500

    Fix language testing on Travis (#22)

--- tests/tests/man-page/test.sh
+++ tests/tests/man-page/test.sh
@@ -13,50 +13,72 @@ fi
 DETOX=$1
 INLINE=$(dirname $DETOX)/inline-detox
 
-locale -a
-
 # ----------------------------------------------------------
 
 INPUT=$(printf "\t \$ &")
 
 # ----------------------------------------------------------
 
-OUTPUT="tab_dollar_and_"
-LANG=C.UTF-8
-LC_ALL=$LANG
-LC_CTYPE=$LANG
+WHICH=C.UTF-8
 
-CHECK=$(echo "$INPUT" | $INLINE -f detoxrc.manpage)
-if [ "$CHECK" != "$OUTPUT" ] ; then
-	echo "Man page is a lie.  LANG=$LANG"
-	locale
-	exit 1
+FOUND=$(locale -a | grep -c '^C[.]')
+
+if [ "$FOUND" -gt 0 ] ; then
+	OUTPUT="tab_dollar_and_"
+	LANG=$WHICH
+	LC_ALL=$WHICH
+	LC_CTYPE=$WHICH
+
+	CHECK=$(echo "$INPUT" | $INLINE -f detoxrc.manpage)
+	if [ "$CHECK" != "$OUTPUT" ] ; then
+		echo "Man page is a lie. LANG=$WHICH"
+		locale
+		exit 1
+	fi
+else
+	echo "skipping LANG=$WHICH ... not installed"
 fi
 
 # ----------------------------------------------------------
 
-OUTPUT="tab_dollar_and_"
-LANG=de_DE.UTF-8
-LC_ALL=$LANG
-LC_CTYPE=$LANG
+WHICH=de_DE.UTF-8
 
-CHECK=$(echo "$INPUT" | $INLINE -f detoxrc.manpage)
-if [ "$CHECK" != "$OUTPUT" ] ; then
-	echo "Man page is a lie.  LANG=$LANG"
-	locale
-	exit 1
+FOUND=$(locale -a | grep -c '^de_DE')
+
+if [ "$FOUND" -gt 0 ] ; then
+	OUTPUT="tab_dollar_and_"
+	LANG=$WHICH
+	LC_ALL=$WHICH
+	LC_CTYPE=$WHICH
+
+	CHECK=$(echo "$INPUT" | $INLINE -f detoxrc.manpage)
+	if [ "$CHECK" != "$OUTPUT" ] ; then
+		echo "Man page is a lie. LANG=$WHICH"
+		locale
+		exit 1
+	fi
+else
+	echo "skipping LANG=$WHICH ... not installed"
 fi
 
 # ----------------------------------------------------------
 
-OUTPUT="tab_money_and_"
-LANG=en_US.UTF-8
-LC_ALL=$LANG
-LC_CTYPE=$LANG
+WHICH=en_US.UTF-8
 
-CHECK=$(echo "$INPUT" | $INLINE -f detoxrc.manpage)
-if [ "$CHECK" != "$OUTPUT" ] ; then
-	echo "Man page is a lie.  LANG=$LANG"
-	locale
-	exit 1
+FOUND=$(locale -a | grep -c '^en_US')
+
+if [ "$FOUND" -gt 0 ] ; then
+	OUTPUT="tab_money_and_"
+	LANG=$WHICH
+	LC_ALL=$WHICH
+	LC_CTYPE=$WHICH
+
+	CHECK=$(echo "$INPUT" | $INLINE -f detoxrc.manpage)
+	if [ "$CHECK" != "$OUTPUT" ] ; then
+		echo "Man page is a lie. LANG=$WHICH"
+		locale
+		exit 1
+	fi
+else
+	echo "skipping LANG=$WHICH ... not installed"
 fi
