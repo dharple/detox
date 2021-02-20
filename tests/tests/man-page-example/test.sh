92d96f539c3fb6f27a9083659d4ad8f4257d4136
Doug Harple <dharple@gmail.com>
2021-02-20 15:47:47 -0500

    Update man pages; add regression tests for config files in man pages (#28, #50)

---
+++ tests/tests/man-page-example/test.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -e
+#
+# Confirm that the sample detoxrcs in detoxrc.5 and detox.tbl.5 parse
+# correctly.
+#
+
+if [ -z "$TESTBASE" ] ; then
+	echo TESTBASE needs to be passed via the env
+	exit 1
+fi
+
+. $TESTBASE/test-functions
+
+DETOX=$1
+
+MANPATH=$(dirname $TESTBASE)/man
+
+sed -n '/START SAMPLE/,/END SAMPLE/p' $MANPATH/detoxrc.5 | sed -e's/^[.]." /# /' | diff - detoxrc.detoxrc.5
+sed -n '/START SAMPLE/,/END SAMPLE/p' $MANPATH/detox.tbl.5 | sed -e's/^[.]." /# /' | diff - detoxrc.detox.tbl.5
+
+$DETOX -f detoxrc.detoxrc.5 -L -v | diff - expected.detoxrc.5.txt
+$DETOX -f detoxrc.detox.tbl.5 -L -v | diff - expected.detox.tbl.5.txt
