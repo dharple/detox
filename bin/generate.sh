4d014b07ebfa6fb427db62434e0af372b6c365fe
Doug Harple <dharple@gmail.com>
2021-02-20 23:59:17 -0500

    Update Makefiles to ensure that make distcheck works (#59)

---
+++ bin/generate.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+#
+# Generate internals
+#
+
+PROJECT_ROOT=$(dirname $(dirname $0))
+
+cd $PROJECT_ROOT
+
+#
+# Generate src/builtin_table.c
+#
+
+SRCDIR=$PROJECT_ROOT/src
+GENERATE=$SRCDIR/generate-builtin-table
+
+if [ ! -x $GENERATE ] ; then
+	echo please build $GENERATE first
+	exit 1
+fi
+
+cp $SRCDIR/builtin_table.c.in $SRCDIR/builtin_table.c
+
+for TABLE in safe iso8859_1 unicode cp1252 ; do
+	echo process builtin $TABLE
+	$GENERATE $PROJECT_ROOT/table/$TABLE.tbl.sample | sed -e"s/NEW/$TABLE/" >> $SRCDIR/builtin_table.c
+done
+
+#
+# Generate man/*.pdf
+#
+
+MANDIR=$PROJECT_ROOT/man
+MANDOC=$(command -v mandoc)
+
+for FILE in $MANDIR/*.[15] ; do
+	echo process man page $(basename $FILE)
+	$MANDOC -T pdf $FILE > $FILE.pdf
+done
