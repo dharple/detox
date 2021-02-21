64c1fc3b78881b95a596a59f94336bddca58ccd4
Doug Harple <dharple@gmail.com>
2021-02-21 15:55:55 -0500

    Add tests for --special (resolves #20)

---
+++ tests/tests/github-issue-0020/test.sh
@@ -0,0 +1,91 @@
+#!/bin/bash -e
+#
+# Tests to confirm GitHub issue #20 is resolved
+#
+# https://github.com/dharple/detox/issues/20
+#
+
+if [ -z "$TESTBASE" ] ; then
+	echo TESTBASE needs to be passed via the env
+	exit 1
+fi
+
+. $TESTBASE/test-functions
+
+DETOX=$1
+
+BASE=/tmp/detoxtest/
+if [ ! -d $BASE ] ; then
+	mkdir $BASE
+fi
+
+# -------------------------------------------------
+
+WORK=$(realpath $(mktemp -d $BASE/test-custom-XXXXXX))
+cd $WORK
+
+SOURCE=fool
+INPUT="hi there"
+OUTPUT=$INPUT
+
+touch "$SOURCE"
+ln -s "$SOURCE" "$INPUT"
+
+$DETOX "$INPUT"
+
+if [ ! -f "$OUTPUT" ] ; then
+	echo "Expected $OUTPUT is not present"
+	exit 1
+fi
+
+# --------------------------------------------------
+
+# same test, but with --special
+
+OUTPUT="hi_there"
+
+$DETOX --special "$INPUT"
+
+if [ ! -f "$OUTPUT" ] ; then
+	echo "Expected $OUTPUT is not present"
+	exit 1
+fi
+
+# --------------------------------------------------
+
+# repeat these tests, in a subdirectory
+
+WORK=$(realpath $(mktemp -d $BASE/test-custom-XXXXXX))
+cd $WORK
+
+mkdir test
+cd test
+
+SOURCE=fool
+INPUT="test/hi there"
+OUTPUT="$INPUT"
+
+touch "$SOURCE"
+cd $WORK
+ln -s "$SOURCE" "$INPUT"
+
+$DETOX -r "$INPUT"
+
+if [ ! -f "$OUTPUT" ] ; then
+	echo "Expected $OUTPUT is not present"
+	exit 1
+fi
+
+# --------------------------------------------------
+
+# same test, but with --special
+
+OUTPUT="test/hi_there"
+
+$DETOX --special -r "$INPUT"
+
+if [ ! -f "$OUTPUT" ] ; then
+	echo "Expected $OUTPUT is not present"
+	exit 1
+fi
+
