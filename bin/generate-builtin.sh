70baf396fc76e08466a31bfc59d7621c36ff172e
a1346054 <36859588+a1346054@users.noreply.github.com>
2021-09-20 20:40:54 +0000

    Use consistent style for 'echo'

--- bin/generate-builtin.sh
+++ bin/generate-builtin.sh
@@ -10,13 +10,13 @@ SRCDIR=$PROJECT_ROOT/src
 GENERATE=$SRCDIR/generate-builtin-table
 
 if [ ! -x "$GENERATE" ] ; then
-	echo please build "$GENERATE" first
+	echo "please build $GENERATE first"
 	exit 1
 fi
 
 cp "$SRCDIR"/builtin_table.c.in "$SRCDIR"/builtin_table.c
 
 for TABLE in safe iso8859_1 unicode cp1252 ; do
-	echo process builtin $TABLE
+	echo "process builtin $TABLE"
 	$GENERATE "$PROJECT_ROOT"/table/$TABLE.tbl | sed -e"s/NEW/$TABLE/" >> "$SRCDIR"/builtin_table.c
 done
