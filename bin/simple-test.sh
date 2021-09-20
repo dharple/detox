0e1678bb99ecaa460dc85bb31e66987d89e72eb4
a1346054 <36859588+a1346054@users.noreply.github.com>
2021-09-20 20:26:44 +0000

    Fix spelling

--- bin/simple-test.sh
+++ bin/simple-test.sh
@@ -16,7 +16,7 @@ DETOX="$PROJECT_ROOT/src/detox"
 DETOXRC="$PROJECT_ROOT/etc/detoxrc"
 
 if [ ! -x "$DETOX" ] ; then
-	echo please complie detox first
+	echo please compile detox first
 	exit 1
 fi
 
